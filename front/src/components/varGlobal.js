74445b4e390b950432d34a1e83583528db26bbc9
Thibault Boutaud <krom@gmx.fr>
2025-02-04 10:04:41 +0100

    back and front

--- front/src/components/varGlobal.js
+++ front/src/components/varGlobal.js
@@ -5,5 +5,5 @@ const db = {
   
   //Variable global qui permet de rajouter le port (car dans la bdd il n'y a que le nom de l'image)
 
-  const varGlobal = `http://localhost:${db.port}`; // https://nom_de_domaine:${db.port}
+  const varGlobal = `http://cuisine.tbuilder.fr`; // https://nom_de_domaine:${db.port}
   export default varGlobal;
